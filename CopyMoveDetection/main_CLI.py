ceabe15eef4f1dbac81e52347ede51d9aec22803
rahmat nazali s <rahmatnazali95@gmail.com>
2018-01-03 14:37:19 +0700

    try english polish

--- CopyMoveDetection/main_CLI.py
+++ CopyMoveDetection/main_CLI.py
@@ -7,10 +7,13 @@ import CopyMoveDetection
 Main Code
 """
 
-
+# to detect all images under a directory, use detect_dir
 # CopyMoveDetection.detect_dir('../testcase_image/', '../testcase_result/', 32)
+
+# to detect single image, use detect
 # CopyMoveDetection.detect('../testcase_image/', '01_barrier_copy.png', '../testcase_result/', blockSize=32)
 
+# example
 CopyMoveDetection.detect('../testcase_image/', '06_horses_copy.png', '../testcase_result/', blockSize=32)
 
 
