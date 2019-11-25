7c3d001096134a7efe3fa43d410c2345bc28b643
rahmatnazali95 <rahmatnazali95@gmail.com>
2019-11-25 16:18:58 +0700

    update example

--- examples/example_01.py
+++ examples/example_01.py
@@ -1,13 +1,13 @@
 from copy_move_detection import detect
 
 # detect original image with image block size of 16
-# detect.detect('assets/', 'dataset_example.png', 'output/', block_size=16)
+# detect.detect('assets/dataset_example.png', 'output/', block_size=16)
 
 # detect original image with image block size of 32
-# detect.detect('assets/', 'dataset_example.png', 'output/', block_size=32)
+# detect.detect('assets/dataset_example.png', 'output/', block_size=32)
 
 # detect attacked image with image block size of 16
-# detect.detect('assets/', 'dataset_example_blur.png', 'output/', block_size=16)
+# detect.detect('assets/dataset_example_blur.png', 'output/', block_size=16)
 
 # detect attacked image with image block size of 32
-detect.detect('assets/', 'dataset_example_blur.png', 'output/', block_size=32)
+detect.detect('assets/dataset_example_blur.png', 'output/', block_size=32)
