c76484042336d39b0a23b1973a67da529a090e7b
Rahmat Nazali Salimi <rahmatnazali95@gmail.com>
2023-02-08 10:24:35 +0700

    Update example.py to reflect the new parameter

--- src/example.py
+++ src/example.py
@@ -1,7 +1,10 @@
 from pimage import copy_move
+from pimage.configuration import Configuration
+
+configuration = Configuration(block_size=32)
 
 fraud_list, ground_truth_image, result_image = copy_move.detect("../assets/dataset_example_blur.png",
-                                                                block_size=32,
+                                                                configuration=configuration,
                                                                 verbose=True)
 
 print(fraud_list)
